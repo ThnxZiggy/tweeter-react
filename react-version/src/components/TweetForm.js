6f46a56de0e5b3fef95a83b5293f0a142aab81d5
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-14 00:59:17 -0400

    add preventDefault

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,21 +1,34 @@
 
 function TweetForm() {
-  
+
   const defaultString = 'What are you humming about?'
-  
+  const  eventHandle = (event) => {
+    event.preventDefault()
+  }
   return (
-    
-    // <section class="error-container"><i class="fas fa-exclamation-triangle"></i>
-    //   <h4></h4> <i class="fas fa-exclamation-triangle"></i>
-    // </section>
+    <div>
+    <section class="error-container"><i class="fas fa-exclamation-triangle"></i>
+      <h4></h4> <i class="fas fa-exclamation-triangle"></i>
+    </section>
 
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form onSubmit = { eventHandle } method="post" action="/tweets" className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder={ defaultString }></textarea>
-        <input type="submit" value="Tweet" className="form__input" />
+        <input onClick = { eventHandle } type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
       </form>
     </section>
+    </div>
+  );
+}
+
+function MyClickableDiv() {
+  return (
+    <input onClick={(event) => {
+      console.log(`The mouse coordinates of this click event are: x: ${event.screenX} and y: ${event.screenY}`);
+      }}
+    >
+    </input>
   );
 }
 
